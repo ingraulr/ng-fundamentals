3bf79dfaebdcee2b8357e3082510b5b91d5ef6c1
Raul Reyes <raul.reyes@tress.com.mx>
2019-08-20 15:33:12 -0700

    routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
-
-import { EventsAppComponent } from  './events-app.component'
-import { EventsListComponent } from './events/events-list.component'
-import { EventThumbnailComponent} from './events/event-thumbnail.component'
-import { NavBarComponent } from  './nav/navbar.component'
-
+import { EventsAppComponent } from './events-app.component';
+import { EventsListComponent } from './events/events-list.component';
+import { EventThumbnailComponent} from './events/event-thumbnail.component';
+import { NavBarComponent } from './nav/navbar.component';
+import { EventService } from './events/shared/event.service';
+import { ToastrService } from './common/toastr.service';
+import { EventDetailsComponent } from './events/event-details/event-details.component';
+import { appRoutes } from './routes';
+import { CreateEventComponent } from './events/create-event.component';
+import { Error404Component } from './errors/404.component'
+import { EventRouteActivator } from './events/event-details/event-route-activator.service'
 @NgModule({
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   declarations: [
     EventsAppComponent,
     EventsListComponent,
     EventThumbnailComponent,
-    NavBarComponent
+    EventDetailsComponent,
+    NavBarComponent,
+    NavBarComponent,
+    CreateEventComponent,
+    Error404Component
+
+
   ],
+  providers: [EventService, ToastrService, EventRouteActivator],
   bootstrap: [EventsAppComponent]
 })
 export class AppModule { }
--- src/app/events/events-list.component.ts
+++ src/app/events/events-list.component.ts
@@ -1,31 +1,37 @@
-import { Component } from '@angular/core'
-
+import { Component, OnInit } from '@angular/core';
+import { EventService } from './shared/event.service';
+import { ToastrService } from '../common/toastr.service';
+declare let toastr
 @Component ({
-  selector: 'events-list',
   template: `
       <div>
         <h1>Upcoming Angular Events</h1>
         <hr/>
-        <event-thumbnail #thumbnail [event]="event1"></event-thumbnail>
+        <div class="row">
+          <div *ngFor="let event of events" class="col-md-5">
+            <event-thumbnail (click)="handleThumbnailClick(event.name)"
+            [event]="event"></event-thumbnail>
+          </div>
+        </div>
       </div>
   `
 
 })
 
-export class EventsListComponent {
+export class EventsListComponent implements OnInit {
+  events:any[]
 
-  event1 = {
-    id: 1 ,
-    name: 'Angular Connect',
-    date: '9/26/2036',
-    time: '10:00 am',
-    price: 599.99,
-    imageUrl: '/assets/images/angularconnect-shield.png',
-    location: {
-      address: '1057 DT',
-      city: 'London',
-      country: 'England'
-    }
+  constructor(private eventService: EventService, private toastr: ToastrService) {
+    this.events = this.eventService.getEvents()
   }
 
+  ngOnInit() {
+    this.events = this.eventService.getEvents()
+
+  }
+
+  handleThumbnailClick(eventName) {
+
+    toastr.success(eventName)
+  }
 }
